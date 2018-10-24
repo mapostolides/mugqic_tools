61960c644871221724ca88b462e78ac0dad6b1d3
mapostolides <mapostolides@ccm-3948-1nml.sickkids.ca>
2018-10-24 15:09:58 -0400

    added modified run_test.sh

--- python-tools/fusiontools/0.1.0/bin/reann_cff_fusion_testing/run_test.sh
+++ python-tools/fusiontools/0.1.0/bin/reann_cff_fusion_testing/run_test.sh
@@ -1,4 +1,5 @@
-source setup.sh
+#!/usr/bin/env bash
 echo USAGE: ../run_test.sh \$1=\*.cff \$2=ens_known_genes.bed human_g1k_v37_decoy.fasta
 #python -m trace --listfuncs ../reann_cff_fusion.py $1 $2 human_g1k_v37_decoy.fasta > test 
-python -m trace --listfuncs ../reann_cff_fusion.py $1 $2 human_g1k_v37_decoy.fasta
+#python -m trace --listfuncs ../reann_cff_fusion.py $1 $2 human_g1k_v37_decoy.fasta
+/Users/mapostolides/miniconda3/envs/pysam/bin/python ../reann_cff_fusion.py $1 $2 human_g1k_v37_decoy.fasta
