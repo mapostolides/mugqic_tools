6f9325c4769ac8c5ff99cc397b186fdde0fd276e
Michael Apostolides <michael.j.apostolides@gmail.com>
2019-04-01 18:21:56 -0400

    added pygeneann-OLD.py

--- python-tools/fusiontools/0.1.0/bin/reann_cff_fusion-TEST2.py
+++ python-tools/fusiontools/0.1.0/bin/reann_cff_fusion-TEST2.py
@@ -2,7 +2,7 @@
 import sys
 #sys.path.append("/hpf/largeprojects/ccmbio/jiangyue/DIPG_analysis_by_samples/Scripts/pygeneann/pygenefusionann")
 #sys.path.append("/hpf/largeprojects/ccmbio/jiangyue/Genap_ccm/pygenefusionann/")
-import pygeneann
+import pygeneann-OLD
 import sequtils
 import pysam
 import argparse
