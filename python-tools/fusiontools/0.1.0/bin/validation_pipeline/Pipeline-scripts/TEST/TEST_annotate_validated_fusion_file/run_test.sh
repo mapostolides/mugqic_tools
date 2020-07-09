bab8d7cb0a3a81ac1476c68f1c078cc0749a30b2
Michael Apostolides <michael.j.apostolides@gmail.com>
2020-07-09 10:40:29 -0400

    git rm python-tools/fusiontools/0.1.0/bin/validation_pipeline

--- python-tools/fusiontools/0.1.0/bin/validation_pipeline/Pipeline-scripts/TEST/TEST_annotate_validated_fusion_file/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source setup.sh
-pipeline_codedir=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/Pipeline-scripts
-#in_file=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/stjude_validation/process_validated_fusions_file/105-SJ-Valid-fusions_COMPARED_St-Jude-fusions-removed_duplicates-removed_brokensamples.valfile
-#out_file=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/stjude_validation/process_validated_fusions_file/105-SJ-Valid-fusions_COMPARED_St-Jude-fusions-removed_duplicates-removed_brokensamples-bedfile_intersected.valfile
-in_file=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/kumar_2016_validation/kumar-validated_fusions_grch37_infofile.tsv
-out_file=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/kumar_2016_validation/kumar-validated_fusions_grch37_infofile.bedfile_intersected.valfile
-python $pipeline_codedir/annotate_validated_fusion_file.py $in_file stjude_5callers $out_file
--- python-tools/fusiontools/0.1.0/bin/validation_pipeline/Pipeline-scripts/TEST/convert_genes_limma/RUN_convert_genes_limma.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-scripts_path=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/Pipeline-scripts
-cff_file=SJHGG136_D.integrate.cff
-test_dir=/hpf/largeprojects/ccmbio/mapostolides/mugqic_tools-my-version/python-tools/fusiontools/0.1.0/bin/validation_pipeline/Pipeline-scripts/TEST/convert_genes_limma
-$scripts_path/rename_cff_file_genes.py $cff_file > $test_dir/SJHGG136_D.integrate.cff.renamed
-
-#/hpf/tools/centos6/R/3.5.1/bin/Rscript $code_dir/convert_genes_limma.R $cff_file
